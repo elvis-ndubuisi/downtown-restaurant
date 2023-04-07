d7a3f40a0b14755c326257642d605c604a08f919
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-07 23:37:24 +0100

    navbar slide issues

--- main.js
+++ main.js
@@ -5,10 +5,33 @@ import { setupCounter } from "./counter.js";
 import registerAnimation, {
   revealContentAfter,
   animateIndexAbout,
+  revealNavMenu,
+  hideNavMenu,
 } from "./animations";
+import Swiper, { EffectCoverflow } from "swiper";
+import "swiper/css";
+
+const navClose = document.getElementById("close");
+const navOpen = document.getElementById("open");
 
 document.addEventListener("DOMContentLoaded", () => {
   registerAnimation();
 });
+
+navClose.addEventListener("click", () => hideNavMenu());
+
+navOpen.addEventListener("click", () => revealNavMenu());
+
 revealContentAfter();
 animateIndexAbout();
+
+const swiper = new Swiper(".nav-menu_slider-container", {
+  direction: "horizontal",
+  loop: true,
+  // centerInsufficientSlides: true,
+  // centeredSlidesBounds: true,
+  centeredSlides: true,
+  spaceBetween: 100,
+  slidesPerView: 2,
+  // initialSlide: 1,
+});
