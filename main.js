aef483694d38f164a1c7b1b386e2bf03f8c5269e
elvis-ndubuisi <provictor.ie@gmail.com>
2023-05-02 19:51:36 +0100

    🍨 Hurray - downtown restaurant 🎊 is a go!

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ import registerAnimation, {
   revealNavMenu,
   hideNavMenu,
 } from "./animations";
-import Swiper from "swiper";
-import "swiper/css";
-
+import { gsap } from "gsap";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+import ScrollTrigger from "gsap/ScrollTrigger";
 const navClose = document.getElementById("close");
 const navOpen = document.getElementById("open");
 const hoverMenus = document.querySelectorAll(".changeFocus-link");
 const sliderItems = document.querySelectorAll(".slider-item");
+
+gsap.registerPlugin(CSSRulePlugin, ScrollTrigger);
 
 hoverMenus.forEach((lk) => {
   lk.addEventListener("mouseover", () => {
@@ -23,24 +25,9 @@ hoverMenus.forEach((lk) => {
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  registerAnimation();
-});
-
 navClose.addEventListener("click", () => hideNavMenu());
 
 navOpen.addEventListener("click", () => revealNavMenu());
 
 revealContentAfter();
 // animateIndexAbout();
-
-const swiper = new Swiper(".nav-menu_slider-container", {
-  direction: "horizontal",
-  loop: true,
-  // centerInsufficientSlides: true,
-  // centeredSlidesBounds: true,
-  centeredSlides: true,
-  spaceBetween: 100,
-  slidesPerView: 2,
-  // initialSlide: 1,
-});
